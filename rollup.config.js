5a700042bd611627f797f03268e074570cdd0761
Levi Zitting <me@levizitting.com>
2021-05-07 21:48:29 -0500

    Added minified versions of each output using terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
-import ts from "@wessberg/rollup-plugin-ts";
-
-const pkg = require('./package.json')
+import ts from '@wessberg/rollup-plugin-ts';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: 'src/index.ts',
-  output: [
-    { dir: 'dist/umd', name: 'isNullOrEmpty', format: 'umd', sourcemap: true, },
-    { dir: 'dist/esm', format: 'esm', sourcemap: true },
-    { dir: 'dist', format: 'cjs', sourcemap: true },
-  ],
-  plugins: [ts()],
+    input: 'src/index.ts',
+    output: [
+        { file: 'dist/umd/index.js', name: 'isNullOrEmpty', format: 'umd', sourcemap: true },
+        { file: 'dist/umd/index.min.js', name: 'isNullOrEmpty', format: 'umd', sourcemap: true, plugins: [terser()] },
+        { file: 'dist/esm/index.js', format: 'esm', sourcemap: true },
+        { file: 'dist/esm/index.min.js', format: 'esm', sourcemap: true, plugins: [terser()] },
+        { file: 'dist/index.js', format: 'cjs', sourcemap: true },
+        { file: 'dist/index.min.js', format: 'cjs', sourcemap: true, plugins: [terser()] },
+    ],
+    plugins: [ts()],
 };
