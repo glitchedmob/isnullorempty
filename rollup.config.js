9f85c5e125fa85aa8b52220c84bcc750cd6981b2
Levi Zitting <me@levizitting.com>
2021-05-07 23:11:14 -0500

    Fix issues with types being generated for .min.js outputs

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,23 @@
 import ts from '@wessberg/rollup-plugin-ts';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-    input: 'src/index.ts',
-    output: [
-        { file: 'dist/umd/index.js', name: 'isNullOrEmpty', format: 'umd', sourcemap: true },
-        { file: 'dist/umd/index.min.js', name: 'isNullOrEmpty', format: 'umd', sourcemap: true, plugins: [terser()] },
-        { file: 'dist/esm/index.js', format: 'esm', sourcemap: true },
-        { file: 'dist/esm/index.min.js', format: 'esm', sourcemap: true, plugins: [terser()] },
-        { file: 'dist/index.js', format: 'cjs', sourcemap: true },
-        { file: 'dist/index.min.js', format: 'cjs', sourcemap: true, plugins: [terser()] },
-    ],
-    plugins: [ts()],
-};
+const browserName = 'isNullOrEmpty';
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            { file: 'dist/umd/index.js', name: browserName, format: 'umd', sourcemap: true },
+            { file: 'dist/esm/index.js', format: 'esm', sourcemap: true },
+            { file: 'dist/index.js', format: 'cjs', sourcemap: true },
+        ],
+        plugins: [ts()],
+    },
+    {
+        input: 'src/index.ts',
+        output: [
+            { file: 'dist/umd/index.min.js', name: browserName, format: 'umd', sourcemap: true, plugins: [terser()] },
+        ],
+        plugins: [ts({ tsconfig: 'tsconfig.min.json' })],
+    },
+];
